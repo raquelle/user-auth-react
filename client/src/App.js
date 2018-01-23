2d8db73123e4a49cb4d2e6fa5c37925a288d7aa1
Raquel Eisele <rocky.eisele@gmail.com>
2018-01-23 12:59:17 -0700

    second commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import Home from './components/Home'
+import NoMatch from './components/NoMatch'
+import NavBar from './components/NavBar'
+import Login from './components/Login'
+import Register from './components/Register'
+import { Switch, Route } from 'react-router-dom'
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route component={NoMatch} />
+          </Switch>
+      </div>
+    )
+  }
+}
+
+export default App
